d305da7de8efed71e638e74ec5493ee8ff06a057
Ailton Mizuki Sato <ailtonjapaitapeva@hotmail.com>
2013-01-28 20:50:00 -0200

    Implementação de ArrayGraph
    Não foi implementado o toString()

--- Graphs/src/AdjacencyArrayGraph.java
+++ Graphs/src/AdjacencyArrayGraph.java
@@ -4,52 +4,132 @@
 // Ailton
 public class AdjacencyArrayGraph implements Graph {
 
+	private int cab[], prox[], peso[];
+	private int pos[];
+	private int numVertex, proxDisponivel;
+	
+	
+	public AdjacencyArrayGraph(int numVertex,int numEdge)
+	{
+		int tam = numVertex + 2*numEdge;
+		this.cab = new int[tam];
+		this.peso = new int[tam];
+		this.prox = new int[tam];
+		this.numVertex = numVertex;
+		this.pos = new int[this.numVertex];
+		for(int i=0; i<this.numVertex; i++)
+		{
+			this.prox[i] = 0;
+			this.cab[i] = i;
+			this.peso[i] = 0;
+			this.pos[i] = i;
+		}
+		this.proxDisponivel = this.numVertex;
+	}
+	
+	
+	
 	@Override
 	public void insertEdge(int node1, int node2, int weight) {
-		// TODO Auto-generated method stub
-
+		
+		if(this.proxDisponivel == this.cab.length)
+		{
+			System.out.println("Nao ha espaco disponivel para a aresta");
+		}
+		else
+		{
+			
+			int ind = this.proxDisponivel++;
+			this.prox[this.cab[node1]] = ind;
+			this.cab[ind] = node2;
+			this.cab[node1] = ind;
+			this.prox[ind] = 0;
+			this.peso[ind] = weight;
+			
+		}
+		
 	}
 
 	@Override
 	public boolean existsEdge(int node1, int node2) {
-		// TODO Auto-generated method stub
+		
+		for(int i= this.prox[node1]; i!=0; i=this.prox[i])
+		{
+			if(cab[i] == node2)
+				return true;
+		}
 		return false;
 	}
-
+  
 	@Override
 	public boolean adjacencyListEmpty(int node1) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return (this.prox[node1] == 0);
+		
 	}
 
 	@Override
 	public Edge adjacencyListFirst(int node1) {
-		// TODO Auto-generated method stub
-		return null;
+		this.pos[node1] = node1;
+		return this.adjacencyNext(node1);
+	
 	}
 
 	@Override
 	public Edge adjacencyNext(int node1) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		this.pos[node1] = this.prox[this.pos[node1]];
+		if(this.pos[node1] == 0)
+			return null;
+		else
+			return new Edge(node1, this.cab[pos[node1]], this.peso[pos[node1]]);
 	}
 
 	@Override
 	public Edge removeEdge(int node1, int node2) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		int i;
+		for(i= node1; this.prox[i] !=0;i= this.prox[i])
+			if(this.cab[this.prox[i]] == node2)
+				break;
+		
+		int ind = this.prox[i];
+		if(this.cab[ind] == node2)
+		{
+			Edge edge = new Edge(node1, node2, this.peso[ind]);
+			this.cab[ind] = this.cab.length;
+			if(this.prox[ind] == 0)
+				this.cab[node1] = i;
+			
+			this.prox[i] = this.prox[ind];
+			
+			return edge;
+			
+		}
+		else
+			return null;
+			
+		
 	}
 
 	@Override
 	public int totalNodes() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.numVertex;
 	}
 
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
-
+		
+		for(int i = 0; i < this.numVertex; i++)
+		{
+			System.out.println("Vertice " + i + ":");
+			for(int j = this.prox[i]; j!=0; j=this.prox[j])
+			{
+				System.out.println(" " + this.cab[j] + " (" + this.peso[j] + ")");
+			}
+		}
+		
+		
 	}
 
 }
